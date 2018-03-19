f79e24a266147e296a987071717ea723626e9510
abysmalsb <simon.balazs@outlook.com>
2018-03-19 23:32:51 +0100

    Autonomous movement with Walabot is working

--- Beer-Tank.py
+++ Beer-Tank.py
@@ -2,16 +2,33 @@
 
 from __future__ import print_function, division
 from math import sin, cos, radians
-import WalabotAPI
 import serial
 import sys
 import time
+from os.path import join
+from imp import load_source
 
-MIN_AMPLITUDE = 0.005
+if sys.platform == 'win32':
+	modulePath = join('C:/', 'Program Files', 'Walabot', 'WalabotSDK',
+		'python', 'WalabotAPI.py')
+elif sys.platform.startswith('linux'):
+	modulePath = join('/usr', 'share', 'walabot', 'python', 'WalabotAPI.py')	 
+
+WalabotAPI = load_source('WalabotAPI', modulePath)
+WalabotAPI.Init()
+
+__author__ = "Balazs Simon"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__details__ = "https://www.hackster.io/Abysmal/beer-tank-20a2ed"
+
+MIN_AMPLITUDE = 0.004
 WALABOT_OBJECT_DIRECTION_RANGE = 18
-WALABOT_OBJECT_DISTANCE_RANGE = 40
+WALABOT_OBJECT_DISTANCE_RANGE = 25
 WALABOT_OBJECT_DISTANCE = 80
 
+""" Command line arguments are used to get the correct serial port and baud rate
+"""
 try:
 	SERIAL_PORT = sys.argv[1]
 	SERIAL_BAUD = int(sys.argv[2])
@@ -23,31 +40,35 @@ class BeerTankApp:
 	def __init__(self):
 		self.wlbt = Walabot()  
 		
-		rMin = 10.0
-		rMax = 200.0
-		rRes = 2.0
-		tMax = 20.0
-		tRes = 10.0
-		pMax = 45.0
-		pRes = 2.0
+		self.rMin = 10.0
+		self.rMax = 150.0
+		self.rRes = 2.0
+		self.tMax = 20.0
+		self.tRes = 10.0
+		self.pMax = 50.0
+		self.pRes = 2.0
 		self.thld = 15.0
-		self.mti = False
+		self.mti = 2	# enable MTI
 		
-		self.rParams = (rMin, rMax, rRes)
-		self.tParams = (-tMax, tMax, tRes)
-		self.pParams = (-pMax, pMax, pRes)
+		self.rParams = (self.rMin, self.rMax, self.rRes)
+		self.tParams = (-self.tMax, self.tMax, self.tRes)
+		self.pParams = (-self.pMax, self.pMax, self.pRes)
 		
 	def run(self):
-	
+		""" Initializing the system. 
+			Connecting to the Arduino --> Identifying it -> Connecting to the Walabot -> Run loop
+		"""
 		try:
 			print("Connecting to the MCU of the tank. port: " + SERIAL_PORT + ", baud rate: " + str(SERIAL_BAUD))
 			self.mcu = SerialController(SERIAL_PORT, SERIAL_BAUD)
 			print("MCU found")
+			""" The validation is quite simple. The Raspberry will send the letter 's' to the Arduino.
+				If it answers with 'OK' then it should use the Beer-Tank.ino or other compatible sketch
+			"""
 			print("Identificating MCU")
 			self.mcu.writeSerialData('s')
 			time.sleep(1)
 			response = self.mcu.readSerialData()
-			
 			print(response)
 			if response != 'OK':
 				print("Identification failed")
@@ -59,8 +80,6 @@ class BeerTankApp:
 			print("Can't connect to the tank's MCU")
 			return 
 
-			
-		
 		if self.wlbt.isConnected():  # connection achieved
 			print ("Connected to Walabot")
 			try:
@@ -68,16 +87,13 @@ class BeerTankApp:
 			except WalabotAPI.WalabotError as err:
 				print(str(err))
 				return
-			#self.wlbt.calibrate()
-			params = self.wlbt.getParameters()
 			print("Status: " + self.wlbt.getStatusString())
 			self.loop()
 		else:
 			print("Walabot is NOT connected")
 	
 	def loop(self):
-		""" Triggers the Walabot, get the Sensor targets, and update the
-			canvas and other components accordingly.
+		""" Giving moving commands to the Arduino based on the targets received from the Walabot
 		"""
 		while True:
 			try:
@@ -87,28 +103,45 @@ class BeerTankApp:
 				self.stopLoop()
 				return
 
-			if len(targets) > 0 and MIN_AMPLITUDE < targets[0].amplitude:
-				target = targets[0]
-				print(target)
-				if abs(target.yPosCm) < WALABOT_OBJECT_DIRECTION_RANGE:
-					print("irány ok")
-					motorSpeedChange = 0;
-				elif target.yPosCm > 0:
-					motorSpeedChange = int(target.yPosCm - WALABOT_OBJECT_DIRECTION_RANGE) * 2
-				else:
-					motorSpeedChange = int(target.yPosCm + WALABOT_OBJECT_DIRECTION_RANGE) * 2
+			if not targets:
+				continue
+			""" There might be multiple targets. Most likely the real target is the one that is the 
+				closest to the goal (the human's desired position) and has a high enough amplitude
+			"""
+			goalZ = self.rMax / 2
+			goalY = 0
+			closest = targets[0]
+			for t in targets:
+				""" Calculating the distance of 't' and the goal
+				"""
+				if ((closest.yPosCm - goalY) ** 2 + (closest.zPosCm - goalZ) ** 2) ** 0.5 > ((t.yPosCm - goalY) ** 2 + (t.zPosCm - goalZ) ** 2) ** 0.5 and t.amplitude > MIN_AMPLITUDE:
+					closest = t				
 
-				if abs(target.zPosCm - WALABOT_OBJECT_DISTANCE) < WALABOT_OBJECT_DISTANCE_RANGE:
-					print("távolság ok")
-					motorSpeedBase = 0;
-				elif target.zPosCm - WALABOT_OBJECT_DISTANCE < 0:
-					motorSpeedBase = int((target.zPosCm - WALABOT_OBJECT_DISTANCE + WALABOT_OBJECT_DISTANCE_RANGE) * 5)
-				else:
-					motorSpeedBase = int((target.zPosCm - WALABOT_OBJECT_DISTANCE - WALABOT_OBJECT_DISTANCE_RANGE) * 5)
+			""" Due to possible false targets and the slow update speed of the Walabot, I used fix 
+				speeds for moving to avoid very fast movement. A minimum speed was also required to
+				make the tank move.
+			"""
+			if 'closest' in locals() and MIN_AMPLITUDE < closest.amplitude:
+				if closest.yPosCm > WALABOT_OBJECT_DIRECTION_RANGE:
+					motorSpeedChange = 75
+					motorSpeedBase = 0
+				elif closest.yPosCm < -WALABOT_OBJECT_DIRECTION_RANGE:
+					motorSpeedChange = -75
+					motorSpeedBase = 0
+				elif closest.zPosCm - WALABOT_OBJECT_DISTANCE > WALABOT_OBJECT_DISTANCE_RANGE:
+					motorSpeedChange = 0
+					motorSpeedBase = 50
+				elif closest.zPosCm - WALABOT_OBJECT_DISTANCE < -WALABOT_OBJECT_DISTANCE_RANGE:
+					motorSpeedChange = 0
+					motorSpeedBase = -50
+			else:
+				motorSpeedChange = 0
+				motorSpeedBase = 0
 					
-				print("speed " + str(motorSpeedBase))
-				print("turn " + str(motorSpeedChange))
-				self.mcu.writeSerialData('m ' + str(motorSpeedBase) + " " + str(motorSpeedChange))
+			print("y: " + str(closest.yPosCm) + " z: " + str(closest.zPosCm)  + " speed: " + str(motorSpeedBase) + " turn: " + str(motorSpeedChange) + " a: " + str(closest.amplitude))
+			""" sending a movement command to the Arduino
+			"""
+			self.mcu.writeSerialData('m ' + str(motorSpeedBase) + " " + str(motorSpeedChange))
 		
 	def stopLoop(self):
 		""" Kills the loop function and reset the relevant app components.
@@ -203,14 +236,18 @@ class SerialController:
 		self.openSerial(port, baud)
 		
 	def getPortBaudRate(self):
-		""" getter
+		""" Getter for port and baud
 		"""
 		return self.port, self.baud
 		
 	def writeSerialData(self, data):
+		""" Sending data to the Arduino through serial communication. Line ending is '\r\n'
+		"""
 		self.serial.write((data + '\r\n').encode())
 		
 	def readSerialData(self):
+		""" Reading the data, received from the Arduino
+		"""
 		data = ''
 		while self.serial.inWaiting() > 0:
 			data += self.serial.read(1).decode("utf-8")
@@ -218,7 +255,7 @@ class SerialController:
 		return data.rstrip("\r").strip()
 	
 	def openSerial(self, port, baud):
-		""" open serial communication
+		""" Open serial communication
 		"""
 		self.port = port
 		self.baud = baud
@@ -237,4 +274,6 @@ class SerialController:
 			self.serial.close()
 		
 if __name__ == "__main__":
+	""" Starting the application
+	"""
 	BeerTankApp().run()
