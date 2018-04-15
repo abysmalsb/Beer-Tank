e739e95d9d22e73b858fcb82476d84012cecd517
abysmalsb <simon.balazs@outlook.com>
2018-04-16 01:39:02 +0200

    fixing issues that prevented the skill from publishing

--- lambda/index.js
+++ lambda/index.js
@@ -3,106 +3,127 @@
 const Alexa = require('alexa-sdk');
 const AWS = require('aws-sdk');
 
+const HELP_MESSAGE = 'You can ask me these: Turn on or off the lights. Start or stop following you. Open or close the cargo hold and you can also say exit. What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const PROCEED = 'How would you like to proceed?';
+const STOP_MESSAGE = 'Goodbye!';
+
 //Environment Configuration
-var config = {};
-config.IOT_BROKER_ENDPOINT      = "a16zyi8b5y4ebz.iot.us-west-2.amazonaws.com".toLowerCase();
-config.IOT_BROKER_REGION        = "us-west-2";
-config.IOT_THING_NAME           = "WalaBeerTank";
+const config = {
+    IOT_THING_NAME: "WalaBeerTank",
+    IOT_BROKER_ENDPOINT: "a16zyi8b5y4ebz.iot.us-west-2.amazonaws.com",
+    IOT_BROKER_REGION: "us-west-2"
+};
 AWS.config.region = config.IOT_BROKER_REGION;
 //Initializing client for IoT
-var iotData = new AWS.IotData({endpoint: config.IOT_BROKER_ENDPOINT});
+var iotData = new AWS.IotData({ endpoint: config.IOT_BROKER_ENDPOINT });
 
 //Payload skeleton
 var mqttPayload = {
     state: {
-        desired : {
-            led : "off",
+        desired: {
+            led: "off",
             crane: "close",
-            follow : "stop"
+            follow: "stop"
         }
     }
-}
+};
 
 //Sending the payload to the AWS IoT and responding to the user with Alexa
 function processing(mqttTopic, alexaResponse, alexa) {
-	var paramsUpdate = {
-            topic: mqttTopic,
-            payload: JSON.stringify(mqttPayload),
-            qos:0
-        };
-        iotData.publish(paramsUpdate, function(err, data) {
-          if (err){
+    var paramsUpdate = {
+        topic: mqttTopic,
+        payload: JSON.stringify(mqttPayload),
+        qos: 0
+    };
+    iotData.publish(paramsUpdate, function(err, data) {
+        if (err) {
             //Handle the error here
             console.log("MQTT Error" + data);
-          }
-          else {
+        }
+        else {
             console.log(data);
-            alexa.emit(":ask", alexaResponse)
-          }    
-        });
+            alexa.response.speak(alexaResponse + " " + PROCEED).listen(alexaResponse + " " + PROCEED);
+            alexa.emit(':responseReady');
+        }
+    });
 }
 
 const handlers = {
 
     'LaunchRequest': function() {
-        this.emit(':ask', 'I\'m ready')
+        const message = 'WalaBeer Tank is ready. ' + HELP_REPROMPT;
+        this.response.speak(message).listen(message);
+        this.emit(':responseReady');
     },
     'LightSwitchIntent': function() {
-        var command = this.event.request.intent.slots.state.value
+        var command = this.event.request.intent.slots.state.value;
         if (command == 'on') {
-			mqttPayload.state.desired.led = command
-			processing("wbt", 'Turning lighting on', this)
+            mqttPayload.state.desired.led = command;
+            processing("wbt", 'Turning lighting on.', this);
         }
         else if (command == 'off') {
-			mqttPayload.state.desired.led = command
-			processing("wbt", 'Turning lighting off', this)
+            mqttPayload.state.desired.led = command;
+            processing("wbt", 'Turning lighting off.', this);
         }
         else {
-            this.emit(":ask", 'I don\'t understand your command')
+            this.response.speak('I don\'t understand your command.').listen('I don\'t understand your command.');
+            this.emit(':responseReady');
         }
     },
     'CargoIntent': function() {
-        var command = this.event.request.intent.slots.cargoState.value
+        var command = this.event.request.intent.slots.cargoState.value;
         if (command == 'open') {
-			mqttPayload.state.desired.crane = command
-			processing("wbt", 'Elevating beer', this)
+            mqttPayload.state.desired.crane = command;
+            processing("wbt", 'Elevating beer.', this);
         }
         else if (command == 'close') {
-			mqttPayload.state.desired.crane = command
-			processing("wbt", 'Closing the cargo hold', this)
+            mqttPayload.state.desired.crane = command;
+            processing("wbt", 'Closing the cargo hold.', this);
         }
         else {
-            this.emit(":ask", 'I don\'t understand your command')
+            this.response.speak('I don\'t understand your command.').listen('I don\'t understand your command.');
+            this.emit(':responseReady');
         }
     },
     'MovementIntent': function() {
-        var command = this.event.request.intent.slots.movementCommand.value
+        var command = this.event.request.intent.slots.movementCommand.value;
         if (command == 'start' || command == 'follow me') {
-			mqttPayload.state.desired.follow = 'start'
-			processing("wbt", 'Beer Tank will follow you', this)
+            mqttPayload.state.desired.follow = 'start';
+            processing("wbt", 'Beer Tank will follow you.', this);
         }
         else if (command == 'stop') {
-			mqttPayload.state.desired.follow = 'stop'
-			processing("wbt", 'Beer Tank stopped', this)
+            mqttPayload.state.desired.follow = 'stop';
+            processing("wbt", 'Beer Tank stopped.', this);
         }
         else {
-            this.emit(":ask", 'I don\'t understand your command ')
+            this.response.speak('I don\'t understand your command.').listen('I don\'t understand your command.');
+            this.emit(':responseReady');
         }
     },
-    "SessionEndedRequest": function() {
-        this.emit()
+    'AMAZON.HelpIntent': function() {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
     },
-    "AMAZON.HelpIntent": function (intent, session, response) {
-        this.emit(":ask", "You can ask me these: Turn on or off the lights. Follow you or stop. Open or close the cargo hold");
+    'AMAZON.CancelIntent': function() {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
     },
-    "Unhandled": function() {
-        this.emit(":ask", 'I don\'t understand your command ')
-    }
-}
+    'AMAZON.StopIntent': function() {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'Unhandled': function() {
+        this.response.speak('I don\'t understand your command.').listen('I don\'t understand your command.');
+        this.emit(':responseReady');
+    },
+};
 
 exports.handler = (event, context, callback) => {
-    
-    var alexa = Alexa.handler(event, context);
+    const alexa = Alexa.handler(event, context, callback);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
